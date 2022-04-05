6073e063e5ab671c6b16e70f4bffed76566924b4
hailm <hai.lemanh.1310@gmail.com>
2022-04-05 23:29:04 +0700

    update

--- template-parts/layouts/block_taiapp.php
+++ template-parts/layouts/block_taiapp.php
@@ -1,10 +1,12 @@
-<div class="uk-section block_taiapp__section uk-light">
-    <div class="uk-container">
-        <div class="item__32">
-            <h2 class="uk-h2 uk-text-center block_cauhoithuonggap__title">Tải ứng ụng app MyVBI</h2>
-        </div>
-        <div class="item__32">
+<div class="block_taiapp__section uk-light">
+    <div class="uk-section uk-background-norepeat uk-background-cover" data-src="images/MaskGroup.png" uk-img>
+        <div class="uk-container">
+            <div class="item__32">
+                <h2 class="uk-h2 uk-text-center block_cauhoithuonggap__title">Tải ứng ụng app MyVBI</h2>
+            </div>
+            <div class="item__32">
 
+            </div>
         </div>
     </div>
 </div>
