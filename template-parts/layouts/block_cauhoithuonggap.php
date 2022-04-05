b8e6afedd23d8d492567aff17a321f207cd3de65
hailm <hai.lemanh.1310@gmail.com>
2022-04-05 21:11:02 +0700

    update

--- template-parts/layouts/block_cauhoithuonggap.php
+++ template-parts/layouts/block_cauhoithuonggap.php
@@ -1,58 +1,85 @@
-<div class="uk-section">
+<div class="uk-section block_cauhoithuonggap__section">
     <div class="uk-container">
-        <h2 class="uk-h2 uk-text-center">Câu hỏi thường gặp</h2>
-        <div uk-grid>
-            <div class="uk-width-1-3@m">
-
-            </div>
-            <div class="uk-width-expand">
-                <div class="item__40">
-                    <ul uk-accordion>
-                        <li class="uk-open">
-                            <a class="uk-accordion-title" href="#">1. Hợp đồng bảo hiểm sẽ được gửi đến khách hàng như thế nào</a>
-                            <div class="uk-accordion-content">
-                                <p>"Khi mua và thanh toán thành công sản phẩm bảo hiểm sức khỏe, khách hàng sẽ được cấp giấy chứng nhận bảo hiểm điện tử do VBI phát hành qua email đã đăng ký.</p>
-                                <p>Giấy chứng nhận điện tử có đầy đủ giá trị pháp lý theo quy định tại Nghị định 52/2013/NĐ-CP ngày 16/05/2013 của Chính phủ về Thương mại điện tử. Khách hàng cũng có thể truy cập về thông tin giấy chứng nhận và quyền lợi bảo hiểm trên App My VBI."</p>
-                            </div>
-                        </li>
-                        <li>
-                            <a class="uk-accordion-title" href="#">2. Ưu điểm của bảo hiểm sức khỏe VBI care là gì?</a>
-                            <div class="uk-accordion-content">
-                                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor reprehenderit.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <a class="uk-accordion-title" href="#">3. Tại sao nên mua bảo hiểm online?</a>
-                            <div class="uk-accordion-content">
-                                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat proident.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <a class="uk-accordion-title" href="#">4. Bảo hiểm VBI care cho phép khám chữa bệnh ở những bệnh viện nào?</a>
-                            <div class="uk-accordion-content">
-                                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat proident.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <a class="uk-accordion-title" href="#">5. Thời gian chờ là gì?</a>
-                            <div class="uk-accordion-content">
-                                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat proident.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <a class="uk-accordion-title" href="#">6. Bệnh có sẵn là gì?</a>
-                            <div class="uk-accordion-content">
-                                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat proident.</p>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-                <div class="item__40">
-                    <div class="item__16">
-                        <div>Bạn đang không tìm thấy câu trả lời mà mình mong đợi?</div>
+        <div class="item__32">
+            <h2 class="uk-h2 uk-text-center block_cauhoithuonggap__title">Câu hỏi thường gặp</h2>
+        </div>
+        <div class="item__32">
+            <div class="uk-grid-small" uk-grid>
+                <div class="uk-width-1-3@m">
+                    <div class="uk-padding-small block_cauhoithuonggap__card uk-visible@m">
+                        <ul class="block_cauhoithuonggap__nav uk-nav-default uk-nav-divider" uk-nav>
+                            <li class="block_cauhoithuonggap__nav__li uk-active"><a class="block_cauhoithuonggap__nav__a" href="#">Câu hỏi về bảo hiểm sức khỏe</a></li>
+                            <li class="block_cauhoithuonggap__nav__li"><a class="block_cauhoithuonggap__nav__a" href="#">Câu hỏi về bảo hiểm xe</a></li>
+                            <li class="block_cauhoithuonggap__nav__li"><a class="block_cauhoithuonggap__nav__a" href="#">Câu hỏi về bảo hiểm khác</a></li>
+                            <li class="block_cauhoithuonggap__nav__li"><a class="block_cauhoithuonggap__nav__a" href="">Câu hỏi về bồi thường</a></li>
+                            <li class="block_cauhoithuonggap__nav__li"><a class="block_cauhoithuonggap__nav__a" href="">Câu hỏi về tái tục</a></li>
+                        </ul>
                     </div>
-                    <div class="item__16">
+                    <div class="uk-hidden@m">
 
+                    </div>
+                </div>
+                <div class="uk-width-expand">
+                    <div class="item__40">
+                        <ul uk-accordion>
+                            <li class="block_cauhoithuonggap__accordion__li uk-open">
+                                <a class="uk-accordion-title block_cauhoithuonggap__accordion__title" href="#">1. Hợp đồng bảo hiểm sẽ được gửi đến khách hàng như thế nào</a>
+                                <div class="uk-accordion-content block_cauhoithuonggap__accordion__content">
+                                    <div class="block_cauhoithuonggap__accordion__box">
+                                        <p>"Khi mua và thanh toán thành công sản phẩm bảo hiểm sức khỏe, khách hàng sẽ được cấp giấy chứng nhận bảo hiểm điện tử do VBI phát hành qua email đã đăng ký.</p>
+                                        <p>Giấy chứng nhận điện tử có đầy đủ giá trị pháp lý theo quy định tại Nghị định 52/2013/NĐ-CP ngày 16/05/2013 của Chính phủ về Thương mại điện tử. Khách hàng cũng có thể truy cập về thông tin giấy chứng nhận và quyền lợi bảo hiểm trên App My VBI."</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="block_cauhoithuonggap__accordion__li">
+                                <a class="uk-accordion-title block_cauhoithuonggap__accordion__title" href="#">2. Ưu điểm của bảo hiểm sức khỏe VBI care là gì?</a>
+                                <div class="uk-accordion-content block_cauhoithuonggap__accordion__content">
+                                    <div class="block_cauhoithuonggap__accordion__box">
+                                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor reprehenderit.</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="block_cauhoithuonggap__accordion__li">
+                                <a class="uk-accordion-title block_cauhoithuonggap__accordion__title" href="#">3. Tại sao nên mua bảo hiểm online?</a>
+                                <div class="uk-accordion-content block_cauhoithuonggap__accordion__content">
+                                    <div class="block_cauhoithuonggap__accordion__box">
+                                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor reprehenderit.</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="block_cauhoithuonggap__accordion__li">
+                                <a class="uk-accordion-title block_cauhoithuonggap__accordion__title" href="#">4. Bảo hiểm VBI care cho phép khám chữa bệnh ở những bệnh viện nào?</a>
+                                <div class="uk-accordion-content block_cauhoithuonggap__accordion__content">
+                                    <div class="block_cauhoithuonggap__accordion__box">
+                                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor reprehenderit.</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="block_cauhoithuonggap__accordion__li">
+                                <a class="uk-accordion-title block_cauhoithuonggap__accordion__title" href="#">5. Thời gian chờ là gì?</a>
+                                <div class="uk-accordion-content block_cauhoithuonggap__accordion__content">
+                                    <div class="block_cauhoithuonggap__accordion__box">
+                                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor reprehenderit.</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="block_cauhoithuonggap__accordion__li">
+                                <a class="uk-accordion-title block_cauhoithuonggap__accordion__title" href="#">6. Bệnh có sẵn là gì?</a>
+                                <div class="uk-accordion-content block_cauhoithuonggap__accordion__content">
+                                    <div class="block_cauhoithuonggap__accordion__box">
+                                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor reprehenderit.</p>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="item__40">
+                        <div class="item__16">
+                            <div class="block_cauhoithuonggap__txt">Bạn đang không tìm thấy câu trả lời mà mình mong đợi?</div>
+                        </div>
+                        <div class="item__16">
+                            <button class="block_cauhoithuonggap__btn uk-button uk-button-default uk-border-pill"><span>Xem thêm câu hỏi thường gặp của chúng tôi</span></button>
+                        </div>
                     </div>
                 </div>
             </div>
