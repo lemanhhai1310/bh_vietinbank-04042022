a9b9ca35cf306a67155ebd78c33e59da764880b6
hailm <hai.lemanh.1310@gmail.com>
2022-04-05 23:25:44 +0700

    update

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php $data["title"] = "Trang chủ"; ?>
 <?php $body = '' ?>
 <?php require "template-parts/layouts/header.php"; ?>
+<?php require "template-parts/layouts/block_taiapp.php"; ?>
 <?php require "template-parts/layouts/block_cauhoithuonggap.php"; ?>
 <?php require "template-parts/layouts/footer.php"; ?>
---
+++ template-parts/layouts/block_taiapp.php
@@ -0,0 +1,10 @@
+<div class="uk-section block_taiapp__section uk-light">
+    <div class="uk-container">
+        <div class="item__32">
+            <h2 class="uk-h2 uk-text-center block_cauhoithuonggap__title">Tải ứng ụng app MyVBI</h2>
+        </div>
+        <div class="item__32">
+
+        </div>
+    </div>
+</div>
