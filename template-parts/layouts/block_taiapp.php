488507c902b1380a1fbbe95aeee43b59ba10bc02
hailm <hai.lemanh.1310@gmail.com>
2022-04-05 23:56:41 +0700

    feat: Tải ứng ụng app MyVBI

--- template-parts/layouts/block_taiapp.php
+++ template-parts/layouts/block_taiapp.php
@@ -1,11 +1,49 @@
-<div class="block_taiapp__section uk-light">
-    <div class="uk-section uk-background-norepeat uk-background-cover" data-src="images/MaskGroup.png" uk-img>
+<div class="block_taiapp__gradient uk-light uk-overflow-hidden">
+    <div class="block_taiapp__section uk-section uk-background-norepeat uk-background-cover" data-src="images/MaskGroup.png" uk-img>
         <div class="uk-container">
             <div class="item__32">
                 <h2 class="uk-h2 uk-text-center block_cauhoithuonggap__title">Tải ứng ụng app MyVBI</h2>
             </div>
             <div class="item__32">
-
+                <div uk-grid>
+                    <div class="uk-width-expand">
+                        <div class="">
+                            <div class="item__32">
+                                <div class="item__16">
+                                    <div class="block_taiapp__name">Tại sao bạn cần tải app My VBI?</div>
+                                </div>
+                                <div class="item__16">
+                                    <ul class="uk-list block_taiapp__list">
+                                        <li class="block_taiapp__list__li">Hỗ trợ bạn mọi nơi và mọi lúc</li>
+                                        <li class="block_taiapp__list__li">Lưu trữ tất cả chính sách bảo hiểm</li>
+                                        <li class="block_taiapp__list__li">Các tiện ích giúp bạn trong cuộc sống</li>
+                                        <li class="block_taiapp__list__li">Chủ động khai báo và theo dõi tiến trình xử lý online</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="item__32">
+                                <div class="uk-flex-middle uk-grid-medium" uk-grid>
+                                    <div class="uk-width-auto">
+                                        <img src="images/qrcode.png" alt="">
+                                    </div>
+                                    <div class="uk-width-expand">
+                                        <div class="uk-flex-middle uk-child-width-auto@s uk-grid-small" uk-grid>
+                                            <div>
+                                                <a href=""><img src="images/googleplay.png" alt=""></a>
+                                            </div>
+                                            <div>
+                                                <a href=""><img src="images/appstore.png" alt=""></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="uk-width-auto@m uk-visible@m">
+                        <img uk-scrollspy="cls: uk-animation-slide-bottom; repeat: true" src="images/intro_app.png" alt="">
+                    </div>
+                </div>
             </div>
         </div>
     </div>
