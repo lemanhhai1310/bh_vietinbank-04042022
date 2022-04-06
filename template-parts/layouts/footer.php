1e5958a4d76c2e62ff3c4a424c16107b8c58b1c0
hailm <hai.lemanh.1310@gmail.com>
2022-04-06 10:28:59 +0700

    update

--- template-parts/layouts/footer.php
+++ template-parts/layouts/footer.php
@@ -1,5 +1,43 @@
-<div class="footer">
+<div class="footer uk-section uk-light">
+    <div class="uk-container">
+        <div uk-grid>
+            <div class="uk-width-1-3@m">
+                <div class="item__30">
+                    <img src="images/logo_f.png" alt="">
+                </div>
+                <div class="item__30">
+                    <div class="uk-grid-small uk-grid-50-s" uk-grid>
+                        <div class="uk-width-1-1@m uk-width-1-2@s">
+                            <ul class="uk-list footer__listAddress">
+                                <li class="footer__listAddress__li footer__listAddress__map">Tầng 10 -11, tòa nhà 126 Đội Cấn Quận Ba Đình, Hà Nội</li>
+                                <li class="footer__listAddress__li footer__listAddress__map">Số 243A Đê La Thành, Láng Thượng, Đống Đa, Hà Nội</li>
+                                <li class="footer__listAddress__li footer__listAddress__phone">1900.1566</li>
+                            </ul>
+                        </div>
+                        <div class="uk-width-1-1@m uk-width-1-2@s">
+                            <div class="item__16">
+                                <div>Tải xuống ứng dụng của VBI</div>
+                            </div>
+                            <div class="item__16">
 
+                            </div>
+                        </div>
+                        <div class="uk-width-1-1@m uk-width-1-2@s uk-visible@m">
+                            <div class="item__16">
+                                <div>Theo dõi VBI qua mạng xã hội</div>
+                            </div>
+                            <div class="item__16">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="uk-width-expand">
+
+            </div>
+        </div>
+    </div>
 </div>
 </div>
 <!--/app-->
