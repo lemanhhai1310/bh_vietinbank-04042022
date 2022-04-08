c812d2c8c6603222bad278f53a9aafa4a3813541
hailm <hai.lemanh.1310@gmail.com>
2022-04-08 22:02:55 +0700

    update

--- template-parts/layouts/footer.php
+++ template-parts/layouts/footer.php
@@ -1,7 +1,7 @@
 <div class="footer uk-section uk-light">
     <div class="uk-container">
-        <div uk-grid>
-            <div class="uk-width-1-3@m">
+        <div class="" uk-grid>
+            <div class="uk-width-auto@m footer__w1">
                 <div class="item__30">
                     <img src="images/logo_f.png" alt="">
                 </div>
@@ -19,7 +19,21 @@
                                 <div>Tải xuống ứng dụng của VBI</div>
                             </div>
                             <div class="item__16">
-
+                                <div class="uk-flex-middle uk-grid-medium" uk-grid>
+                                    <div class="uk-width-auto">
+                                        <img src="images/qrcode.png" alt="">
+                                    </div>
+                                    <div class="uk-width-expand">
+                                        <div class="uk-flex-middle uk-grid-10" uk-grid>
+                                            <div>
+                                                <a href=""><img src="images/googleplay.png" alt=""></a>
+                                            </div>
+                                            <div>
+                                                <a href=""><img src="images/appstore.png" alt=""></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div class="uk-width-1-1@m uk-width-1-2@s uk-visible@m">
@@ -34,7 +48,110 @@
                 </div>
             </div>
             <div class="uk-width-expand">
-
+                <div class="item__32">
+                    <div class="uk-child-width-1-4@m" uk-grid>
+                        <div>
+                            <div class="item__12">
+                                <div class="footer__title2">Công ty</div>
+                            </div>
+                            <div class="item__12">
+                                <ul class="footer__nav uk-nav uk-nav-default">
+                                    <li><a href="#">Về chúng tôi</a></li>
+                                    <li><a href="#">Ban giám đốc</a></li>
+                                    <li><a href="#">Cơ hội nghề nghiệp</a></li>
+                                    <li><a href="#">Quan hệ đối tác</a></li>
+                                    <li><a href="#">Sơ đồ trang web</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="item__12">
+                                <div class="footer__title2">Hỗ trợ</div>
+                            </div>
+                            <div class="item__12">
+                                <ul class="footer__nav uk-nav uk-nav-default">
+                                    <li><a href="">Dịch vụ khách hàng</a></li>
+                                    <li><a href="">Yêu cầu bồi thường</a></li>
+                                    <li><a href="">Câu hỏi thường gặp</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="item__12">
+                                <div class="footer__title2">Tiện ích</div>
+                            </div>
+                            <div class="item__12">
+                                <ul class="footer__nav uk-nav uk-nav-default">
+                                    <li><a href="">Mã thanh toán</a></li>
+                                    <li><a href="">Garage gần bạn</a></li>
+                                    <li><a href="">Bệnh viện bảo lãnh</a></li>
+                                    <li><a href="">Liên hệ CSGT</a></li>
+                                    <li><a href="">Trung tâm đăng kiểm</a></li>
+                                    <li><a href="">Trung tâm cứu hộ</a></li>
+                                    <li><a href="">VBI Gần bạn</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item__32">
+                    <div class="item__16">
+                        <div class="footer__title2">Sản phẩm bảo hiểm</div>
+                    </div>
+                    <div class="item__16">
+                        <div uk-grid>
+                            <div class="uk-width-expand">
+                                <div class="item__16">
+                                    <div class="footer__title3">Sản phẩm cho bạn và gia đình</div>
+                                </div>
+                                <div class="item__16">
+                                    <div class="uk-child-width-1-3@m" uk-grid>
+                                        <div>
+                                            <ul class="footer__nav uk-nav uk-nav-default">
+                                                <li><a href="">Bảo hiểm Sức khỏe</a></li>
+                                                <li><a href="">Bảo hiểm ung thư</a></li>
+                                                <li><a href="">Bảo hiểm ung thư vú</a></li>
+                                                <li><a href="">Bảo hiểm vì cộng đồng</a></li>
+                                                <li><a href="">Bảo hiểm Tai nạn con người</a></li>
+                                                <li><a href="">Bảo hiểm sốt xuất huyết</a></li>
+                                            </ul>
+                                        </div>
+                                        <div>
+                                            <ul class="footer__nav uk-nav uk-nav-default">
+                                                <li><a href="">Bảo hiểm vật chất ô tô</a></li>
+                                                <li><a href="">Bảo hiểm vật chất xe máy</a></li>
+                                                <li><a href="">Bảo hiểm TNDS ô tô</a></li>
+                                                <li><a href="">Bảo hiểm TNDS xe máy</a></li>
+                                            </ul>
+                                        </div>
+                                        <div>
+                                            <ul class="footer__nav uk-nav uk-nav-default">
+                                                <li><a href="">Bảo hiểm nhà tư nhân</a></li>
+                                                <li><a href="">Bảo hiểm du lịch nội địa</a></li>
+                                                <li><a href="">Bảo hiểm du lịch quốc tế</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="uk-width-1-4@m">
+                                <div class="item__16">
+                                    <div class="footer__title3">Sản phẩm doanh nghiệp</div>
+                                </div>
+                                <div class="item__16">
+                                    <ul class="footer__nav uk-nav uk-nav-default">
+                                        <li><a href="">Sức khỏe nhân viên</a></li>
+                                        <li><a href="">Bảo hiểm xe cơ giới</a></li>
+                                        <li><a href="">Bảo hiểm tàu biển</a></li>
+                                        <li><a href="">Bảo vệ công trình xây dựng</a></li>
+                                        <li><a href="">Bảo hiểm trách nhiệm</a></li>
+                                        <li><a href="">Bảo hiểm năng lượng</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
