3e56b5f5a9c64fb91e762775b0b2a74c0f2b8097
hailm <hai.lemanh.1310@gmail.com>
2022-04-09 08:10:11 +0700

    update

--- index.php
+++ index.php
@@ -96,19 +96,57 @@
 
                     <div class="uk-position-relative">
 
-                        <div class="uk-slider-container uk-light">
-                            <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-4@m uk-grid-small" uk-grid>
+                        <div class="uk-slider-container">
+                            <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-6@m uk-grid-small" uk-grid>
                                 <?php
                                 $data = array(
                                     array(
-                                        'src' => '',
-                                        'txt' => '',
+                                        'src' => 'images/baovebanvagiadinh/heat.png',
+                                        'txt' => 'Bảo vệ cho sức khỏe',
+                                    ),
+                                    array(
+                                        'src' => 'images/baovebanvagiadinh/xecogioi.png',
+                                        'txt' => 'Bảo vệ xe cơ giới',
+                                    ),
+                                    array(
+                                        'src' => 'images/baovebanvagiadinh/medical-report1.png',
+                                        'txt' => 'Bảo vệ trước bệnh hiểm nghèo',
+                                    ),
+                                    array(
+                                        'src' => 'images/baovebanvagiadinh/tainan.png',
+                                        'txt' => 'Bảo vệ trước tai nạn',
+                                    ),
+                                    array(
+                                        'src' => 'images/baovebanvagiadinh/airplane1.png',
+                                        'txt' => 'Bảo vệ khi đi du lịch',
+                                    ),
+                                    array(
+                                        'src' => 'images/baovebanvagiadinh/vaccine1.png',
+                                        'txt' => 'Bảo vệ trước dịch bệnh',
+                                    ),
+                                    array(
+                                        'src' => 'images/baovebanvagiadinh/best-friend1.png',
+                                        'txt' => 'Bảo vệ cho trẻ nhỏ',
+                                    ),
+                                    array(
+                                        'src' => 'images/baovebanvagiadinh/heat.png',
+                                        'txt' => 'Bảo vệ cho sức khỏe',
+                                    ),
+                                    array(
+                                        'src' => 'images/baovebanvagiadinh/heat.png',
+                                        'txt' => 'Bảo vệ cho sức khỏe',
                                     ),
                                 );
                                 foreach ($data as $k=>$v): ?>
-                                <li>
-                                    <div class="uk-padding-small">
-
+                                <li class="home__block02__w7">
+                                    <div class="uk-cover-container uk-padding-small home__block02__box">
+                                        <canvas width="142" height="142"></canvas>
+                                        <div class="uk-position-cover uk-padding-small uk-flex uk-flex-column uk-text-center">
+                                            <div class="uk-flex-1 uk-flex uk-flex-middle uk-flex-center">
+                                                <img src="<?= $v['src'] ?>" alt="">
+                                            </div>
+                                            <div class="uk-flex-1 home__block01__txt"><?= $v['txt'] ?></div>
+                                        </div>
                                     </div>
                                 </li>
                                 <?php endforeach; ?>
@@ -137,12 +175,174 @@
                 <div class="home__block02__title">Bảo vệ doanh nghiệp của bạn</div>
             </div>
             <div class="item__16">
+                <div uk-slider>
 
+                    <div class="uk-position-relative">
+
+                        <div class="uk-slider-container">
+                            <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-6@m uk-grid-small" uk-grid>
+                                <?php
+                                $data = array(
+                                    array(
+                                        'src' => 'images/baovebanvagiadinh/heat.png',
+                                        'txt' => 'Bảo vệ cho sức khỏe',
+                                    ),
+                                    array(
+                                        'src' => 'images/baovebanvagiadinh/xecogioi.png',
+                                        'txt' => 'Bảo vệ xe cơ giới',
+                                    ),
+                                    array(
+                                        'src' => 'images/baovebanvagiadinh/medical-report1.png',
+                                        'txt' => 'Bảo vệ trước bệnh hiểm nghèo',
+                                    ),
+                                    array(
+                                        'src' => 'images/baovebanvagiadinh/tainan.png',
+                                        'txt' => 'Bảo vệ trước tai nạn',
+                                    ),
+                                    array(
+                                        'src' => 'images/baovebanvagiadinh/airplane1.png',
+                                        'txt' => 'Bảo vệ khi đi du lịch',
+                                    ),
+                                    array(
+                                        'src' => 'images/baovebanvagiadinh/vaccine1.png',
+                                        'txt' => 'Bảo vệ trước dịch bệnh',
+                                    ),
+                                    array(
+                                        'src' => 'images/baovebanvagiadinh/best-friend1.png',
+                                        'txt' => 'Bảo vệ cho trẻ nhỏ',
+                                    ),
+                                    array(
+                                        'src' => 'images/baovebanvagiadinh/heat.png',
+                                        'txt' => 'Bảo vệ cho sức khỏe',
+                                    ),
+                                    array(
+                                        'src' => 'images/baovebanvagiadinh/heat.png',
+                                        'txt' => 'Bảo vệ cho sức khỏe',
+                                    ),
+                                );
+                                foreach ($data as $k=>$v): ?>
+                                    <li class="home__block02__w7">
+                                        <div class="uk-cover-container uk-padding-small home__block02__box">
+                                            <canvas width="142" height="142"></canvas>
+                                            <div class="uk-position-cover uk-padding-small uk-flex uk-flex-column uk-text-center">
+                                                <div class="uk-flex-1 uk-flex uk-flex-middle uk-flex-center">
+                                                    <img src="<?= $v['src'] ?>" alt="">
+                                                </div>
+                                                <div class="uk-flex-1 home__block01__txt"><?= $v['txt'] ?></div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </div>
+
+                        <div class="uk-hidden@s uk-light">
+                            <a class="uk-position-center-left uk-position-small" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+                            <a class="uk-position-center-right uk-position-small" href="#" uk-slidenav-next uk-slider-item="next"></a>
+                        </div>
+
+                        <div class="uk-visible@s">
+                            <a class="uk-position-center-left-out uk-position-small" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+                            <a class="uk-position-center-right-out uk-position-small" href="#" uk-slidenav-next uk-slider-item="next"></a>
+                        </div>
+
+                    </div>
+
+                    <ul class="uk-slider-nav uk-dotnav uk-flex-center uk-margin uk-hidden"></ul>
+
+                </div>
             </div>
         </div>
     </div>
 </div>
 <!--/Bạn muốn bảo vệ gì hôm nay ?-->
+<!--Tiện ích 24/7-->
+<div class="uk-section uk-background-muted">
+    <div class="uk-container">
+        <div class="item__32">
+            <h2 class="uk-h2 uk-text-center block_cauhoithuonggap__title">Tiện ích 24/7</h2>
+        </div>
+        <div class="item__32">
+            <div uk-slider>
+
+                <div class="uk-position-relative">
+
+                    <div class="uk-slider-container">
+                        <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-6@m uk-grid-small" uk-grid>
+                            <?php
+                            $data = array(
+                                array(
+                                    'src' => 'images/baovebanvagiadinh/heat.png',
+                                    'txt' => 'Bảo vệ cho sức khỏe',
+                                ),
+                                array(
+                                    'src' => 'images/baovebanvagiadinh/xecogioi.png',
+                                    'txt' => 'Bảo vệ xe cơ giới',
+                                ),
+                                array(
+                                    'src' => 'images/baovebanvagiadinh/medical-report1.png',
+                                    'txt' => 'Bảo vệ trước bệnh hiểm nghèo',
+                                ),
+                                array(
+                                    'src' => 'images/baovebanvagiadinh/tainan.png',
+                                    'txt' => 'Bảo vệ trước tai nạn',
+                                ),
+                                array(
+                                    'src' => 'images/baovebanvagiadinh/airplane1.png',
+                                    'txt' => 'Bảo vệ khi đi du lịch',
+                                ),
+                                array(
+                                    'src' => 'images/baovebanvagiadinh/vaccine1.png',
+                                    'txt' => 'Bảo vệ trước dịch bệnh',
+                                ),
+                                array(
+                                    'src' => 'images/baovebanvagiadinh/best-friend1.png',
+                                    'txt' => 'Bảo vệ cho trẻ nhỏ',
+                                ),
+                                array(
+                                    'src' => 'images/baovebanvagiadinh/heat.png',
+                                    'txt' => 'Bảo vệ cho sức khỏe',
+                                ),
+                                array(
+                                    'src' => 'images/baovebanvagiadinh/heat.png',
+                                    'txt' => 'Bảo vệ cho sức khỏe',
+                                ),
+                            );
+                            foreach ($data as $k=>$v): ?>
+                                <li class="home__block02__w7">
+                                    <div class="uk-cover-container uk-padding-small home__block02__box">
+                                        <canvas width="142" height="142"></canvas>
+                                        <div class="uk-position-cover uk-padding-small uk-flex uk-flex-column uk-text-center">
+                                            <div class="uk-flex-1 uk-flex uk-flex-middle uk-flex-center">
+                                                <img src="<?= $v['src'] ?>" alt="">
+                                            </div>
+                                            <div class="uk-flex-1 home__block01__txt"><?= $v['txt'] ?></div>
+                                        </div>
+                                    </div>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+
+                    <div class="uk-hidden@s uk-light">
+                        <a class="uk-position-center-left uk-position-small" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+                        <a class="uk-position-center-right uk-position-small" href="#" uk-slidenav-next uk-slider-item="next"></a>
+                    </div>
+
+                    <div class="uk-visible@s">
+                        <a class="uk-position-center-left-out uk-position-small" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+                        <a class="uk-position-center-right-out uk-position-small" href="#" uk-slidenav-next uk-slider-item="next"></a>
+                    </div>
+
+                </div>
+
+                <ul class="uk-slider-nav uk-dotnav uk-flex-center uk-margin uk-hidden"></ul>
+
+            </div>
+        </div>
+    </div>
+</div>
+<!--/Tiện ích 24/7-->
 <?php require "template-parts/layouts/block_danhgiakhachhangdoitac.php"; ?>
 <?php require "template-parts/layouts/block_taiapp.php"; ?>
 <?php require "template-parts/layouts/block_cauhoithuonggap.php"; ?>
