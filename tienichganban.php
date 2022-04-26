4e4c88bb9ac84b7b821617f19d55ddc180f21d9a
hailm <hai.lemanh.1310@gmail.com>
2022-04-26 21:56:40 +0700

    feat: res page tienichganban

--- tienichganban.php
+++ tienichganban.php
@@ -20,7 +20,7 @@ require "template-parts/layouts/breadcrumb.php"; ?>
             <h2 class="uk-h2 uk-text-center block_cauhoithuonggap__title">Bệnh viện gần bạn</h2>
         </div>
         <div class="item__32">
-            <div class="uk-grid-small uk-grid-match" uk-grid>
+            <div class="uk-grid-small uk-grid-match uk-visible@m" uk-grid>
                 <div class="uk-width-1-4@m">
                     <div class="uk-flex uk-flex-column">
                         <div class="tienichganban__box item__15">
@@ -123,6 +123,148 @@ require "template-parts/layouts/breadcrumb.php"; ?>
                                     </div>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="uk-hidden@m">
+                <div class="item__24">
+                    <div class="tienichganban__box2">
+                        <div class="uk-child-width-1-3@s uk-grid-small" uk-grid>
+                            <div>
+                                <div class="uk-search uk-search-default uk-width-1-1">
+                                    <span class="uk-search-icon-flip" uk-search-icon></span>
+                                    <input class="tienichganban__box__input uk-search-input uk-width-1-1" type="search" placeholder="Tìm tên bệnh viện">
+                                </div>
+                            </div>
+                            <div>
+                                <div class="uk-width-1-1" uk-form-custom="target: > * > span:first-child">
+                                    <select>
+                                        <option value="">Thành phố</option>
+                                        <option value="1">Option 01</option>
+                                        <option value="2">Option 02</option>
+                                        <option value="3">Option 03</option>
+                                        <option value="4">Option 04</option>
+                                    </select>
+                                    <button class="tienichganban__box__btn uk-width-1-1 uk-position-relative uk-button uk-button-default" type="button" tabindex="-1">
+                                        <span></span>
+                                        <span class="uk-position-center-right" uk-icon="icon: chevron-down"></span>
+                                    </button>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="uk-width-1-1" uk-form-custom="target: > * > span:first-child">
+                                    <select>
+                                        <option value="">Quận huyện</option>
+                                        <option value="1">Option 01</option>
+                                        <option value="2">Option 02</option>
+                                        <option value="3">Option 03</option>
+                                        <option value="4">Option 04</option>
+                                    </select>
+                                    <button class="tienichganban__box__btn uk-width-1-1 uk-position-relative uk-button uk-button-default" type="button" tabindex="-1">
+                                        <span></span>
+                                        <span class="uk-position-center-right" uk-icon="icon: chevron-down"></span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item__24">
+                    <div class="uk-grid-17" uk-grid>
+                        <div class="uk-width-auto@s">
+                            <div class="tienichganban__boxMap__title">Tiện ích trên VBI</div>
+                        </div>
+                        <div class="uk-width-expand">
+                            <div class="uk-child-width-auto uk-grid-8" uk-grid>
+                                <?php
+                                $data = array(
+                                    array(
+                                        'src' => 'images/tienichganban/icon1.png',
+                                        'txt' => 'Bệnh viện',
+                                    ),
+                                    array(
+                                        'src' => 'images/tienichganban/icon2.png',
+                                        'txt' => 'Garage ô tô',
+                                    ),
+                                    array(
+                                        'src' => 'images/tienichganban/icon3.png',
+                                        'txt' => 'VBI thành viên',
+                                    ),
+                                    array(
+                                        'src' => 'images/tienichganban/icon4.png',
+                                        'txt' => 'Trung tâm đăng kiểm xe',
+                                    ),
+                                    array(
+                                        'src' => 'images/tienichganban/icon5.png',
+                                        'txt' => 'Trung tâm cứu hộ xe',
+                                    ),
+                                );
+                                foreach ($data as $k=>$v): ?>
+                                    <div>
+                                        <a href="" class="<?= ($k==0)?'uk-active':'' ?> tienichganban__boxMap__btn uk-button uk-button-default uk-border-pill" data-src="<?= $v['src'] ?>" uk-img><span><?= $v['txt'] ?></span></a>
+                                    </div>
+                                <?php endforeach; ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item__24">
+                    <div class="uk-cover-container tienichganban__box3">
+                        <canvas width="696" height="509"></canvas>
+                        <iframe class="uk-position-cover" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d29804.115456811083!2d105.9468519770752!3d20.972006242412082!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1svi!2s!4v1650201079644!5m2!1svi!2s" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+                <div class="item__24">
+                    <div class="uk-visible@s uk-overflow-auto tienichganban__box4" style="max-height: 448px">
+                        <table class="uk-table uk-table-divider uk-table-small uk-margin-remove">
+                            <tbody>
+                            <?php for ($i=0;$i<=15;$i++): ?>
+                            <tr>
+                                <td>
+                                    <div class="uk-grid-8 uk-flex-middle" uk-grid>
+                                        <div class="uk-width-auto">
+                                            <img class="tienichganban__box1__img" src="images/tienichganban/icon1.png" alt="">
+                                        </div>
+                                        <div class="uk-width-expand">
+                                            <div class="tienichganban__box1__title">Bệnh viện đa khoa Việt Đức</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <ul class="uk-list tienichganban__box1__list">
+                                        <li>162A Tôn Đức Thắng, Hàng Bột</li>
+                                    </ul>
+                                </td>
+                                <td>
+                                    <ul class="uk-list tienichganban__box1__list">
+                                        <li class="tel">0977788899</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <?php endfor; ?>
+                            </tbody>
+                        </table>
+
+                    </div>
+                    <div class="uk-hidden@s">
+                        <div class="tienichganban__box item__15 uk-flex-auto uk-overflow-auto" style="max-height: 424px">
+                            <?php for ($i=1;$i<=6;$i++): ?>
+                                <div class="tienichganban__box__item">
+                                    <div class="uk-grid-8" uk-grid>
+                                        <div class="uk-width-auto">
+                                            <img class="tienichganban__box1__img" src="images/tienichganban/icon1.png" alt="">
+                                        </div>
+                                        <div class="uk-width-expand">
+                                            <div class="tienichganban__box1__title">Bệnh viện đa khoa Việt Đức</div>
+                                            <ul class="uk-list tienichganban__box1__list">
+                                                <li>162A Tôn Đức Thắng, Hàng Bột</li>
+                                                <li>0977788899</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php endfor; ?>
                         </div>
                     </div>
                 </div>
