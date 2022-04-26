cca8c0625576c20f163fff2856c3e3b2d80e248d
hailm <hai.lemanh.1310@gmail.com>
2022-04-26 22:37:26 +0700

    fix: fix menu scroll

--- template-parts/layouts/navbar-menu.php
+++ template-parts/layouts/navbar-menu.php
@@ -1,9 +1,6 @@
 <nav class="uk-navbar-container cocauquantri__navbar" uk-navbar>
-    <div class="uk-navbar-left">
-        <a href="" class="uk-navbar-item"></a>
-    </div>
-    <div class="uk-navbar-center">
-        <ul class="uk-navbar-nav">
+    <div class="uk-navbar-left uk-flex-1 uk-width-1-1 uk-flex-center">
+        <ul class="uk-navbar-nav uk-flex-nowrap uk-overflow-auto" style="width: max-content">
             <?php if (isset($datamenu)): ?>
                 <?php foreach ($datamenu as $k=>$v): ?>
                     <li class="<?= ($v['active'])?'uk-active':'' ?>"><a href="<?= $v['link'] ?>"><?= $v['txt'] ?></a></li>
